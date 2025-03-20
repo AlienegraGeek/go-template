eec6d74b45badf87cef7e061cf841f9f72cf2b63
AlienegraGeek <969024145@qq.com>
2025-03-20 23:55:25 +0800

    chore(pkg): update

--- pkg/random.go
+++ pkg/random.go
@@ -1,25 +1,10 @@
 package pkg
 
 import (
-	"fmt"
 	"math/rand"
 	"strconv"
-	"strings"
 	"time"
 )
-
-// 随机数字
-func RandomNumber(width int) string {
-	numeric := [10]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	r := len(numeric)
-	rand.Seed(time.Now().UnixNano())
-
-	var sb strings.Builder
-	for i := 0; i < width; i++ {
-		fmt.Fprintf(&sb, "%d", numeric[rand.Intn(r)])
-	}
-	return sb.String()
-}
 
 const alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
