47fb9fa854c2d49a3dc2894f922daa289c65fb05
AlienegraGeek <969024145@qq.com>
2025-03-22 23:59:22 +0800

    build(mod): bump dependencies version

--- pkg/random.go
+++ pkg/random.go
@@ -2,8 +2,6 @@ package pkg
 
 import (
 	"math/rand"
-	"strconv"
-	"time"
 )
 
 const alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -25,23 +23,4 @@ func RandomMaxString(n int) string {
 		b[i] = alphanumerics2[rand.Int63()%int64(len(alphanumerics2))]
 	}
 	return string(b)
-}
-
-const as = "136145481759273241471451054914857641823742613747"
-
-func RandomFloat64(n int) float64 {
-	b := make([]byte, n)
-	rand.Seed(time.Now().UnixNano())
-	for i := range b {
-		if i == 2 {
-			b[i] = '0'
-		} else if i == 1 {
-			b[i] = '.'
-		} else {
-			b[i] = as[(rand.Intn(len(as)))]
-		}
-
-	}
-	float, _ := strconv.ParseFloat(string(b), 4)
-	return float
 }
